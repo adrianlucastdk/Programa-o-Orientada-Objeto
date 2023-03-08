8af8a722e8fb74b5a593d4063a5cf62704766d0d
adrianlucastdk <101604988+adrianlucastdk@users.noreply.github.com>
2023-03-08 19:55:57 -0300

    Add files via upload

---
+++ newreactjs.jsx
@@ -0,0 +1,35 @@
+public class Animal 
+{
+    public static void main(String[] args)
+    {
+            Gato gato1 = new Gato()
+            gato1.nome = "Marley"
+            gato1.cor = "Marrom"
+            gato1.peso = "14"
+            gato1.raca = "Beagle"
+            
+            Gato gato2 = new Gato()
+            gato2.nome = "Caramelo"
+            gato2.cor = "Preto"
+            gato2.peso = "19"
+            gato2.raca = "Akita"
+            
+            Gato gato3 = new Gato()
+            gato3.nome = "Maggie"
+            gato3.cor = "Branco"
+            gato3.peso = "22"
+            gato3.raca = "Boxer"
+            
+            Gato gato4 = new Gato()
+            gato4.nome = "Peppa"
+            gato4.cor = "Preto"
+            gato4.peso = "17"
+            gato4.raca = "Pastor alemão"
+            
+            Gato gato5 = new Gato()
+            gato4.nome = "Haven"
+            gato4.cor = "Cinza"
+            gato4.peso = "8"
+            gato4.raca = "Rottweiler"
+    }
+}
