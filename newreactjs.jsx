94c17703625b38d691a2ddc90f16b2b2cb2de182
adrianlucastdk <101604988+adrianlucastdk@users.noreply.github.com>
2023-03-08 20:01:09 -0300

    Update newreactjs.jsx

--- newreactjs.jsx
+++ newreactjs.jsx
@@ -7,29 +7,34 @@ public class Animal
             gato1.cor = "Marrom"
             gato1.peso = "14"
             gato1.raca = "Beagle"
+            gato1.idade = 13
             
             Gato gato2 = new Gato()
             gato2.nome = "Caramelo"
             gato2.cor = "Preto"
             gato2.peso = "19"
             gato2.raca = "Akita"
+            gato1.idade = 8
             
             Gato gato3 = new Gato()
             gato3.nome = "Maggie"
             gato3.cor = "Branco"
             gato3.peso = "22"
             gato3.raca = "Boxer"
+            gato1.idade = 18
             
             Gato gato4 = new Gato()
             gato4.nome = "Peppa"
             gato4.cor = "Preto"
             gato4.peso = "17"
             gato4.raca = "Pastor alemão"
+             gato1.idade = 19
             
             Gato gato5 = new Gato()
             gato4.nome = "Haven"
             gato4.cor = "Cinza"
             gato4.peso = "8"
             gato4.raca = "Rottweiler"
+            gato1.idade = 5
     }
 }
