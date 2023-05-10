1fc2da304993a6d88f4d817eb26d95096997ec79
adrianlucastdk <101604988+adrianlucastdk@users.noreply.github.com>
2023-05-10 19:43:15 -0300

    Add files via upload
    Override

---
+++ Exemploaula1005/src/main/java/com/mycompany/exemploaula1005/Cachorro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemploaula1005;
+
+/**
+ *
+ * @author aluno
+ */
+class Cachorro extends Animal {
+    @Override
+    public void emitirSom(){
+        System.out.println("O cachorro latiu. ");
+    }
+}
---
+++ Exemploaula1005/src/main/java/com/mycompany/exemploaula1005/Exemploaula1005.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemploaula1005;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exemploaula1005 {
+
+    public static void main(String[] args) {
+       Animal animal = new Cachorro();
+       animal.emitirSom();
+    }
+}
